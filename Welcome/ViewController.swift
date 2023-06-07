028d66b2f7db555c829660e5024c92a7fd4c3889
WiproAssaignment <pavanios2016@gmail.com>
2023-06-07 18:24:43 +0530

    change the border color
    when give the border color in UIbutton

--- Welcome/ViewController.swift
+++ Welcome/ViewController.swift
@@ -13,9 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureBorder()
         
     }
-   
+    
+    func configureBorder() {
+        buttonLabel.layer.borderColor = UIColor.gray.cgColor
+        buttonLabel.layer.borderWidth = 1.0
+    }
 
 }
 
